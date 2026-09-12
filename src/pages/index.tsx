dabd94d27ba3ea0cc4a9ddc9fefb5f0897a218f2
SessionHu <sessionhu@outlook.com>
2026-09-13 05:34:28 +0800

    feat: add logo, partners and sales

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import type React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
 
 interface FeatureCardProps {
@@ -11,6 +13,44 @@ const FeatureCard = ({ title, children }: FeatureCardProps) => {
       <h3>{title}</h3>
       <p>{children}</p>
     </section>
+  );
+};
+
+const PartnersList: React.FC = () => {
+  const pl: [string, string, any?][] = [
+    ['p', 'https://dn42.li/dn42.svg', {height:'200%',transform:'translateY(-25%)'}],
+    ['p', 'https://raw.githubusercontent.com/NeoCloud/NeoNetwork/master/logo.png'],
+    ['p', 'https://peeringdb-media-prod.s3.amazonaws.com/media/logos_user_supplied/network-33599-7fa2825b.jpg', {height:'80%'}],
+    ['p', 'https://peeringdb-media-prod.s3.amazonaws.com/media/logos_user_supplied/organization-42473-dc42bfbc.png', {height:'80%'}],
+    ['p', 'https://dn42.burble.com/burble-dn42-64.png', {height:'30%'}],
+    ['p', 'https://iedon.net/favicon.ico'],
+    ['p', 'https://blog.sherpherd.net/img/sernet-logo-small.svg'],
+    ['p', 'https://avatars.githubusercontent.com/u/88564180?s=200&v=4'],
+    ['p', 'https://nosla42.pmman.tech/storage/logo-light.webp'],
+    ['p', 'https://he.net/images/helogo.gif', {height:'40%'}],
+    ['p', 'https://cloud.nbdnet.com/storage/logo-light.webp', {height:'50%'}],
+    ['p', 'https://cogentco.com/files/images/c2020/cogentco_logo.png', {height:'40%'}],
+    ['p', 'https://static.xhustudio.eu.org/images/svg/CF_logomark.svg', {height:'40%'}],
+    ['p', 'https://intranet.services/favicon.svg'],
+    ['p', 'https://szsyzx.cn/assets/icon.svg'],
+    ['p', 'https://www.constant.com/favicons/favicon-96.png', {height:'80%'}],
+  ];
+  for (let i = pl.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [pl[i], pl[j]] = [pl[j], pl[i]];
+  }
+	return (
+    <div className={styles.partnersList}>
+      <ul>
+        {pl.concat(pl).map((e, i) => {
+          return (
+            <li key={i}>{
+              e[0] === 'p' ? <img src={e[1]} style={e[2]} /> : e[1]
+            }</li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
@@ -28,18 +68,21 @@ export default () => {
       </section>
       <section>
         <h2>Services</h2>
-        <p><b>Software Development: </b>...</p>
-        <p><b>Network Solutions: </b>...</p>
-        <p><b>Open Infrastructure: </b>...</p>
+        <p><b>Software Development: </b>Over 6 years of development experience. High performance and cross-platform and device compatibility.</p>
+        <p><b>Network Solutions: </b>Top participant in the dn42 internet and well-known for excellent network quality and architecture.</p>
+        <p><b>Open Infrastructure: </b>Open and transparent infrastructure help you defend your security and freedom.</p>
       </section>
       <section>
         <h2>Partners</h2>
+        <PartnersList />
       </section>
       <section>
         <h2>Feedbacks</h2>
       </section>
       <section>
         <h2>Sales</h2>
+        <p>Matrix Group: <Link to="https://matrix.to/#/!YwqwnCuNtSOyWOCytE:matrix.org">!YwqwnCuNtSOyWOCytE:matrix.org</Link></p>
+        <p>E-Mail: <Link to="mailto:sales@sess.moe">sales@sess.moe</Link></p>
       </section>
     </>
   )
